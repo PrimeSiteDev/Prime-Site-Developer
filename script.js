cf597ad7e2522845aadb581017c1fa6c8a12b4ed
PrimeSiteDev <PrimeSiteDeveloper@gmail.com>
2026-04-16 19:24:19 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll",()=>{
-document.querySelectorAll(".section").forEach(el=>{
-let top = el.getBoundingClientRect().top;
-if(top < window.innerHeight - 100){
-el.style.opacity = 1;
-el.style.transform = "translateY(0)";
-}
-});
-});
